981cc32b21409a6dbe685049fcbdb0973736a515
Shahid Afridi <115124671+Shahidafridi3303@users.noreply.github.com>
2024-11-04 16:35:55 -0500

    Added GameManager script for Tic Tac Toe game with Netcode. handled when client get connected then show the GameCanvas

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public NetworkVariable<int> currentTurn = new NetworkVariable<int>(0);
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+
+    public void StartClient()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+
+    private async void Start()
+    {
+        NetworkManager.Singleton.OnClientConnectedCallback += (clientId) =>
+        {
+            Debug.Log("Client with id " + clientId + " joined");
+            if (NetworkManager.Singleton.IsHost &&
+                NetworkManager.Singleton.ConnectedClients.Count == 2)
+            {
+                SpwanBoard();
+            }
+        };
+    }
+
+    [SerializeField] private GameObject boardPrefab;
+    private void SpwanBoard()
+    {
+        GameObject newBoard = Instantiate(boardPrefab);
+        newBoard.GetComponent<NetworkObject>().Spawn();
+    }
+}
