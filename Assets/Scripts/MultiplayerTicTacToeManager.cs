014962efbd4ef3e4ec33f9ab75f021d1f802c808
Shahid Afridi <115124671+Shahidafridi3303@users.noreply.github.com>
2024-11-11 01:26:36 -0500

    Created Text Ui for inform player about states

--- Assets/Scripts/MultiplayerTicTacToeManager.cs
+++ Assets/Scripts/MultiplayerTicTacToeManager.cs
@@ -17,7 +17,9 @@ public class MultiplayerTicTacToeManager : NetworkBehaviour
 
     [SerializeField] private TMP_InputField passwordInputField;
     [SerializeField] private GameObject passwordEntryUI;
+
     [SerializeField] private GameObject leaveButton;
+    [SerializeField] private TextMeshProUGUI Message;
 
     private string serverPassword = "defaultPassword";
 
@@ -62,15 +64,79 @@ public class MultiplayerTicTacToeManager : NetworkBehaviour
         response.Approved = approveConnection;
     }
 
+    private void OnDestroy()
+    {
+        // Prevent error in the editor
+        if (NetworkManager.Singleton == null) { return; }
+
+        NetworkManager.Singleton.OnServerStarted -= HandleServerStarted;
+        NetworkManager.Singleton.OnClientConnectedCallback -= HandleClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback -= HandleClientDisconnect;
+    }
+
     private async void Start()
     {
         // Setup client connection events for board generation
         NetworkManager.Singleton.OnClientConnectedCallback += OnClientJoined;
+        NetworkManager.Singleton.OnClientConnectedCallback += HandleClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback += HandleClientDisconnect;
+    }
+
+    private void HandleServerStarted()
+    {
+        // Temporary workaround to treat host as client
+        if (NetworkManager.Singleton.IsHost)
+        {
+            HandleClientConnected(NetworkManager.Singleton.LocalClientId);
+        }
+    }
+
+    private void HandleClientConnected(ulong clientId)
+    {
+        // Are we the client that is connecting?
+        if (clientId == NetworkManager.Singleton.LocalClientId)
+        {
+            Message.gameObject.SetActive(false);
+
+            passwordEntryUI.SetActive(false);
+            leaveButton.SetActive(true);
+        }
+    }
+
+    private void HandleClientDisconnect(ulong clientId)
+    {
+        Message.gameObject.SetActive(true);
+        Message.text = "You left, Rejoin using Code or create new";
+
+        // Are we the client that is disconnecting?
+        if (clientId == NetworkManager.Singleton.LocalClientId)
+        {
+            passwordEntryUI.SetActive(true);
+            leaveButton.SetActive(false);
+        }
+    }
+
+    public void Leave()
+    {
+        if (NetworkManager.Singleton.IsHost)
+        {
+            NetworkManager.Singleton.ConnectionApprovalCallback -= ApprovalCheck;
+        }
+
+        // Shutdown the network manager to stop the host or client.
+        NetworkManager.Singleton.Shutdown();
+
+        // Re-enable UI elements for entering the game
+        passwordEntryUI.SetActive(true);
+        leaveButton.SetActive(false);
     }
 
     private void OnClientJoined(ulong clientId)
     {
         Debug.Log("Player " + clientId + " connected");
+
+        Message.gameObject.SetActive(true);
+        Message.text = "Waiting for the other player to join!";
 
         // Start game if two players are connected
         if (NetworkManager.Singleton.IsHost && NetworkManager.Singleton.ConnectedClients.Count == 2)
@@ -81,6 +147,9 @@ public class MultiplayerTicTacToeManager : NetworkBehaviour
 
     private void InitializeGameBoard()
     {
+        Message.gameObject.SetActive(false);
+
+        // Instantiate and spawn the board
         currentBoardInstance = Instantiate(boardPrefab);
         currentBoardInstance.GetComponent<NetworkObject>().Spawn();
     }
@@ -93,6 +162,7 @@ public class MultiplayerTicTacToeManager : NetworkBehaviour
             resultPanel.SetActive(true);
             NotifyOpponent("You Lose");
         }
+
         else if (result == "tie")
         {
             resultText.text = "It's a Draw!";
@@ -100,7 +170,6 @@ public class MultiplayerTicTacToeManager : NetworkBehaviour
             NotifyOpponent("It's a Draw!");
         }
     }
-
 
     private void NotifyOpponent(string msg)
     {
@@ -123,7 +192,6 @@ public class MultiplayerTicTacToeManager : NetworkBehaviour
         resultText.text = msg;
         resultPanel.SetActive(true);
     }
-
 
     [ServerRpc(RequireOwnership = false)]
     private void NotifyServerResultServerRpc(string msg)
